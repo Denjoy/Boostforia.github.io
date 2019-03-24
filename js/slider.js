33c6cd229f3d88b89233298fa4f7b3521ec77fc7
Denys Pogurskyi <pogurskyidenys@gmail.com>
2019-03-24 16:06:32 +0100

    full

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,13 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n){
-	showSlides(slideIndex += n);
-}
-function currentSlide(n){
-	showSlides(slideIndex = n);
-}
-function showSlide(n){
-	var i;
-	var slides = document.getElementByClassName("work_slider_information");
-	var links = document.getElementByClassName("nav-item-work");
-	if(n > slides.length){slideIndex = 1}
-	if(n < 1){slideIndex = slides.length}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i= 0; i < links.length; i++) {
-		links[i].className = links[i].className.replace(" active", "")
-	}
-	slides[slideIndex-1].style.display = "block";
-	links[slideIndex-1].className += " active"
-}
+$(function() {
+		var selectedClass = "";
+		$(".fil-cat").click(function(){ 
+		selectedClass = $(this).attr("data-rel"); 
+     $("#portfolio").fadeTo(100, 0.1);
+		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300); 
+		
+	});
+});
