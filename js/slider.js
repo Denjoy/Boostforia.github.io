b9917ececa2249538e344ce55ee11df66e4cb1b0
Denys Pogurskyi <pogurskyidenys@gmail.com>
2019-03-25 12:54:07 +0100

    full fix

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,23 @@
-$(function() {
-		var selectedClass = "";
-		$(".fil-cat").click(function(){ 
-		selectedClass = $(this).attr("data-rel"); 
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300); 
-		
-	});
+$(document).ready(function(){
+
+    $(".fil-cat").click(function(){
+        var value = $(this).attr('data-rel');
+        
+        if(value == "all")
+        {
+            $('.tile').show('1000');
+        }
+        else
+        {
+            $(".tile").not('.'+value).hide('3000');
+            $('.tile').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".fil-cat").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
 });
